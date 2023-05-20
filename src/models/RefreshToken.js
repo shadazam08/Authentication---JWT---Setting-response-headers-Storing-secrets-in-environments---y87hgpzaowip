b79dee19173180d5fbb3dd8c708595b0435ad199
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-05-20 14:03:23 +0530

    Update RefreshToken.js

--- src/models/RefreshToken.js
+++ src/models/RefreshToken.js
@@ -2,7 +2,11 @@ const mongooose = require('mongoose');
 
 const tokenSchema = new mongooose.Schema({
     // Your code goes here
-})
+    token: {
+    type: String,
+    required: true,
+  },
+});
 
 const RefreshToken = mongooose.model('tokens', tokenSchema);
 
